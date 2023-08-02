48244921e55269a84d8eab4b0a21f92a9f41a202
hiyak423 <hiyako0123@icloud.com>
2023-08-02 19:17:57 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+  
+  with_options presence: true do
+    validates :nickname
+    # 全角全角（漢字・ひらがな・カタカナ） format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    # 全角（カタカナ） format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 }
+  end
 end
