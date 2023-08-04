e67900daf4258fc5ecb8ab8ab0fdfc820f590e66
hiyak423 <hiyako0123@icloud.com>
2023-08-04 18:37:19 +0900

    正規表現の修正
    氏名（全角）で登録できない文字があったため

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,8 @@ class User < ApplicationRecord
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龠々]/, message: 'Input full-width characters' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龠々]/, message: 'Input full-width characters' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'Input full-width characters' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Input full-width katakana characters' }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Input full-width katakana characters' }
   end
