c5ed1d2b460808fa189074c9951cc71dc6ee1c85
hiyak423 <hiyako0123@icloud.com>
2023-08-06 19:09:49 +0900

    テスト用 商品データ生成
    テスト実施のため

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name               { Faker::Name.initials }
+    description        { '商品の説明' }
+    category_id        { 2 }
+    status_id          { 2 }
+    shipping_cost_id   { 2 }
+    prefecture_id      { 2 }
+    shipping_day_id    { 2 }
+    price              { 2000 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/item-sample.png'), filename: 'item-sample.png')
+    end
   end
 end
