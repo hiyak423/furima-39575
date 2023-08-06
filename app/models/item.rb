b503e4cf28b88a6e03ac9367d1d7f1d7c2fade0d
hiyak423 <hiyako0123@icloud.com>
2023-08-06 16:28:33 +0900

    商品情報のバリデーション設定
    空の項目があると出品できない
    金額の最低値と上限を設定するため

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :image, :name, :description
+    
+    with_options numericality: { other_than: 1 } do
+      validates :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_day_id
+    end
+
+    with_options format: { with: /\A[0-9]+\z/ } do
+      validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
+    end
+  end
 end
