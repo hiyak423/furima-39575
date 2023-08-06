8885f11090c165da7c387d2d05301db18759e53b
hiyak423 <hiyako0123@icloud.com>
2023-08-06 20:58:20 +0900

    エラーメッセージを表示する
    商品登録失敗時にエラーメッセージを表示するため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,10 +10,11 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
+    if @item.valid?
+      @item.save
+           .redirect_to root_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
