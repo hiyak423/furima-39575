ae1dcdbc96385b2c40f6dbfc40d1978cfa701a96
hiyak423 <hiyako0123@icloud.com>
2023-08-07 15:38:55 +0900

    価格のバリデーション変更
    小数点以下の切り捨て、
    300と9_999_999 を含めるため

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,8 +17,6 @@ class Item < ApplicationRecord
       validates :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_day_id
     end
 
-    with_options format: { with: /\A[0-9]+\z/ } do
-      validates :price, numericality: { greater_than: 300, less_than: 9_999_999 }
-    end
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 end
