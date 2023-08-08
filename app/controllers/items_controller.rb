c71bce99f7ae465115734def8736b2f101e66066
hiyak423 <hiyako0123@icloud.com>
2023-08-08 21:23:29 +0900

    destroyアクション再定義
    間違い修正のため

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,13 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
+    @item.destroy
     redirect_to root_path
-    else
-      render :edit
-    end
   end
-
+  
   private
 
   def item_params
