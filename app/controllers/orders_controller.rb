be726581de80d0553c9f21513e4ebb19ba28a4ac
hiyak423 <hiyako0123@icloud.com>
2023-08-10 20:33:29 +0900

    コントローラ orders作成
    index、createアクション定義のため

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,25 @@
 class OrdersController < ApplicationController
+
   def index
     @item = Item.find(params[:item_id])
+    @order_shipping = OrderShipping.new
   end
+    
 
   def create
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+    
+  private
+
+  def order_params
+    params.require(:order_shipping).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
