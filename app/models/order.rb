5c7d0b86622600169a4b0bcf138e85938821f53b
hiyak423 <hiyako0123@icloud.com>
2023-08-10 13:50:00 +0900

    アソシエーション設定

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
-  
+  has_one :shipping
+  belongs_to :item
+  belongs_to :user
 end
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,2 +1,4 @@
 class Shipping < ApplicationRecord
+  belongs_to :prefecture
+  belongs_to :order  
 end
