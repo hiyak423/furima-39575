540c07f2b144ea1535281366968a4428ef098505
hiyak423 <hiyako0123@icloud.com>
2023-08-11 11:28:56 +0900

    配送先入力でエラーメッセージ表示

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -7,12 +7,11 @@ class OrderShipping
     validates :user_id
     validates :item_id
 
-    validates :postcode
-    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)'}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :block
-    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number'}
   end
 
   def save
