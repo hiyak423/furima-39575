54f486572dfa4029fd220238740dd75424aa2dfc
hiyak423 <hiyako0123@icloud.com>
2023-08-12 12:18:51 +0900

    決済処理の定義、トークンの制限・関連付け

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new
   end
@@ -10,6 +11,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new(order_params)
     if @order_shipping.valid?
+      pay_item
       @order_shipping.save
       redirect_to root_path
     else
@@ -20,7 +22,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_shipping).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+    amount: @item.price,  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,11 +1,12 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id, :city, :block, :building, :phone_number, :token
 
 # バリデーションをかく
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
 
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)'}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
