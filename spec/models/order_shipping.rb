fd74d91bad35dcb92f9eed7747e0c36207adf0f1
hiyak423 <hiyako0123@icloud.com>
2023-08-12 18:04:52 +0900

    商品購入のテスト実施

--- spec/models/order_shipping.rb
+++ spec/models/order_shipping.rb
@@ -15,7 +15,11 @@ RSpec.describe OrderShipping, type: :model do
         expect(@order_shipping).to be_valid
       end
       it '電話番号が10桁以上11桁以内の半角数値のみのとき' do
-        @order_shipping.phone_number = '1234567890'
+        @order_shipping.phone_number = '12345678901'
+        expect(@order_shipping).to be_valid
+      end
+      it '番地が空欄' do
+        @order_shipping.building = ''
         expect(@order_shipping).to be_valid
       end
     end
@@ -38,22 +42,22 @@ RSpec.describe OrderShipping, type: :model do
       it '郵便番号にハイフンが含まれていない' do
         @order_shipping.postcode = "1234567"
         @order_shipping.valid?
-        expect(@order_shipping.errors.full_messages).to include("Postcode is invalid")
+        expect(@order_shipping.errors.full_messages).to include("Postcode is invalid. Enter it as follows (e.g. 123-4567)")
       end
       it '郵便番号が「3桁ハイフン4桁」の半角文字列ではない(1234-5678)' do
         @order_shipping.postcode = "1234-5678"
         @order_shipping.valid?
-        expect(@order_shipping.errors.full_messages).to include("Postcode is invalid")
+        expect(@order_shipping.errors.full_messages).to include("Postcode is invalid. Enter it as follows (e.g. 123-4567)")
       end
       it '郵便番号が「3桁ハイフン4桁」の半角文字列ではない(123-567)' do
         @order_shipping.postcode = "123-567"
         @order_shipping.valid?
-        expect(@order_shipping.errors.full_messages).to include("Postcode is invalid")
+        expect(@order_shipping.errors.full_messages).to include("Postcode is invalid. Enter it as follows (e.g. 123-4567)")
       end
       it '郵便番号が半角文字列ではない(全角の場合)' do
         @order_shipping.postcode = "１２３-４５６７"
         @order_shipping.valid?
-        expect(@order_shipping.errors.full_messages).to include("Postcode is invalid")
+        expect(@order_shipping.errors.full_messages).to include("Postcode is invalid. Enter it as follows (e.g. 123-4567)")
       end
       it '都道府県が空欄' do
         @order_shipping.prefecture_id = nil
@@ -73,27 +77,27 @@ RSpec.describe OrderShipping, type: :model do
       it '電話番号が空欄' do
         @order_shipping.phone_number = ""
         @order_shipping.valid?
-        expect(@order_shipping.errors.full_messages).to include("Phone number can't be blank")
-      end
-      it '電話番号が11桁以上(12桁の場合)' do
-        @order_shipping.phone_number = "123456789012"
-        @order_shipping.valid?
-        expect(@order_shipping.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_shipping.errors.full_messages).to include("Phone number is invalid. Input only number")
       end
       it '電話番号が10桁以下(9桁の場合)' do
         @order_shipping.phone_number = "123456789"
         @order_shipping.valid?
-        expect(@order_shipping.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_shipping.errors.full_messages).to include("Phone number is invalid. Input only number")
       end
       it '電話番号にハイフンが含まれる' do
         @order_shipping.phone_number = "029-777-777"
         @order_shipping.valid?
-        expect(@order_shipping.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_shipping.errors.full_messages).to include("Phone number is invalid. Input only number")
       end
       it '電話番号が半角文字列ではない(全角の場合)' do
         @order_shipping.phone_number = "１２３４５６７８９０１"
         @order_shipping.valid?
-        expect(@order_shipping.errors.full_messages).to include("Phone number is invalid")
+        expect(@order_shipping.errors.full_messages).to include("Phone number is invalid. Input only number")
+      end
+      it 'トークンが空欄' do
+        @order_shipping.token = ""
+        @order_shipping.valid?
+        expect(@order_shipping.errors.full_messages).to include("Token can't be blank")
       end
     end
   end
