2a630fc9da2f3131df31e3fe0c2e2278cac8ad23
hiyak423 <hiyako0123@icloud.com>
2023-08-13 18:51:25 +0900

    モデルを修正
    shippingにorder変数のid情報を保存するため

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -16,8 +16,8 @@ class OrderShipping
   end
 
   def save
-    order = Order.create(user_id: user.id, item_id: item.id)
-    Shipping.create(order_id: order_id, postcode: postcode, prefecture_id: prefecture_id, city: city, block: block,
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Shipping.create(order_id: order.id, postcode: postcode, prefecture_id: prefecture_id, city: city, block: block,
                     building: building, phone_number: phone_number)
   end
 end
